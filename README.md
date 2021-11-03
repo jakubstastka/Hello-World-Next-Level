994c56fb929296eb6cb00978cfaaad2b168775f5
Jakub Stastka <jakub.stastka@leadspicker.com>
2021-11-03 17:45:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+HELLO WORLD NEXT LEVEL
+======================
+
+### What is this?
+The Hello, World project is universally accepted as a ritual to appease the gods of programming. It usually takes a line or two. Or three. That is not a lot. 
+
+I decided to take a different approach and over-engineer the Hello, World script to the maximal ridiculousness level I can deliver.
+
+### But James, why?
+Exactly.
+
+### [this] or [that] is inefficient and could be written in a better way!
+Yes. Probably. What's your point?
+
+### Do you feel this is an achievement?
+That is a valid question, and I applaud you for it.
+
+### I feel like I can complicate this project even more.
+Please do and let me know.
---
+++ hello_world.py
@@ -0,0 +1,51 @@
+class HelloWorld:
+    ALPHABET = []
+    HELLO_WORLD_LETTERS = ['d', 'e', 'h', 'l', 'o', 'r', 'w']
+
+    def __init__(self):
+        alphabet_user_input = input("Please press every alphabetic character key on your keyboard:\n")
+        self.ALPHABET.extend(list(set(alphabet_user_input)))
+
+    @staticmethod
+    def get_letter_indexes(letter, input_list):
+        try:
+            return input_list.index(letter)
+        except AttributeError as error:
+            print(error)
+
+    @staticmethod
+    def assign_letter(letter_dict, looking_for, tuple_index=1):
+        for letter in letter_dict.items():
+            if letter[tuple_index] == looking_for:
+                return letter[tuple_index]
+
+    @classmethod
+    def get_letters(cls):
+        letters_indexes = {}
+
+        for letter in cls.HELLO_WORLD_LETTERS:
+            letter_index = HelloWorld.get_letter_indexes(letter, HelloWorld.ALPHABET)
+            letters_indexes.update({letter_index: letter})
+
+        d = HelloWorld.assign_letter(letters_indexes, "d")
+        e = HelloWorld.assign_letter(letters_indexes, "e")
+        h = HelloWorld.assign_letter(letters_indexes, "h")
+        l = HelloWorld.assign_letter(letters_indexes, "l")
+        o = HelloWorld.assign_letter(letters_indexes, "o")
+        r = HelloWorld.assign_letter(letters_indexes, "r")
+        w = HelloWorld.assign_letter(letters_indexes, "w")
+
+        return d, e, h, l, o, r, w
+
+    def print_hello_world(self):
+        d, e, h, l, o, r, w = HelloWorld.get_letters()
+        hello = f"{h}{e}{l}{l}{o}"
+        world = f"{w}{o}{r}{l}{d}"
+        hello_world = f"{hello.capitalize()}, {world.capitalize()}"
+
+        print(hello_world)
+
+
+if __name__ == "__main__":
+    hw = HelloWorld()
+    hw.print_hello_world()
